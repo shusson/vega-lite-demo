a464528f6bf5bd63a8f22661f51b88b4e0ca7d1f
Shane Husson <shane.a.husson@gmail.com>
2017-08-04 12:28:51 +1000

    add basic vega chart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as embed from 'vega-embed';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+    ngOnInit() {
+        const spec = {
+            "$schema": "https://vega.github.io/schema/vega-lite/v2.json",
+            "description": "A simple bar chart with embedded data.",
+            "data": {
+                "values": [
+                    {"a": "A", "b": 28}, {"a": "B", "b": 55}, {"a": "C", "b": 43},
+                    {"a": "D", "b": 91}, {"a": "E", "b": 81}, {"a": "F", "b": 53},
+                    {"a": "G", "b": 19}, {"a": "H", "b": 87}, {"a": "I", "b": 52}
+                ]
+            },
+            "mark": "bar",
+            "encoding": {
+                "x": {"field": "a", "type": "ordinal"},
+                "y": {"field": "b", "type": "quantitative"}
+            }
+        };
+
+        embed("#vis", spec);
+    }
+
 }
