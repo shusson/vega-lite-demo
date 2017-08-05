6446e8210968bb3456b35bbccc3bf1fd2409db7e
Shane Husson <shane.a.husson@gmail.com>
2017-08-05 17:57:52 +1000

    disable links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,22 +11,22 @@ export class AppComponent implements OnInit {
     ngOnInit() {
         const spec = {
             "$schema": "https://vega.github.io/schema/vega-lite/v2.json",
-            "description": "A simple bar chart with embedded data.",
+            "description": "Simple bar chart",
             "data": {
                 "values": [
-                    {"a": "A", "b": 28}, {"a": "B", "b": 55}, {"a": "C", "b": 43},
-                    {"a": "D", "b": 91}, {"a": "E", "b": 81}, {"a": "F", "b": 53},
-                    {"a": "G", "b": 19}, {"a": "H", "b": 87}, {"a": "I", "b": 52}
+                    {"x": "A", "y": 28}, {"x": "y", "B": 55}, {"x": "C", "y": 43},
+                    {"x": "D", "y": 91}, {"x": "E", "y": 81}, {"x": "F", "y": 53},
+                    {"x": "G", "y": 19}, {"x": "H", "y": 87}, {"x": "I", "y": 52}
                 ]
             },
             "mark": "bar",
             "encoding": {
-                "x": {"field": "a", "type": "ordinal"},
-                "y": {"field": "b", "type": "quantitative"}
+                "x": {"field": "x", "type": "ordinal"},
+                "y": {"field": "y", "type": "quantitative"}
             }
         };
 
-        embed("#vis", spec);
+        embed("#vis", spec, { actions: false });
     }
 
 }
